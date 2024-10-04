2efe7f97bc6c20a7d69155abaf0cf39fe4274b8e
thiagomaquito <50684640+thiagomaquito@users.noreply.github.com>
2024-10-04 00:02:43 -0300

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -1,6 +1,3 @@
-const browser = await puppeteer.launch({
-  args: ['--no-sandbox', '--disable-setuid-sandbox'],
-});
 // leitor de qr code
 const qrcode = require('qrcode-terminal');
 const { Client, Buttons, List, MessageMedia } = require('whatsapp-web.js'); // Mudança Buttons
